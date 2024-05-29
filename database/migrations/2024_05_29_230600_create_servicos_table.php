5523aad0142330069e31e86d8b2bcdd3298c6e0f
anderson <andersonpayao018@gmail.com>
2024-05-29 20:36:47 -0300

    j

---
+++ database/migrations/2024_05_29_230600_create_servicos_table.php
@@ -0,0 +1,44 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('servicos', function (Blueprint $table) {
+            $table->id();
+            $table->string("titulo", 100);
+            $table->text("descricao");
+            $table->decimal("valor");
+            $table->integer("qtde_votos");
+            $table->integer("total_votos");
+            $table->string("telefone", 20);
+            $table->string("celular", 20);
+            $table->string("endereco");
+            $table->string("numero", 10);
+            $table->string("complemento" , 45);
+            $table->string("bairro", 80);
+            $table->string("cidade", 80);
+            $table->string("estado",2);
+            $table->string("cep", 45);
+            $table->foreignId("usuario_id")->constrained();
+            $table->foreignId("categoria_id")->constrained();
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('servicos');
+    }
+};
