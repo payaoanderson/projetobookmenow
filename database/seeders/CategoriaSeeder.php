46fa0600248036a5bc3f043e4f30ad77778a95dd
anderson <andersonpayao018@gmail.com>
2024-05-29 21:46:47 -0300

    seed da tabela categoria e usuario

---
+++ database/seeders/CategoriaSeeder.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Faker\Factory as Faker;
+
+class CategoriaSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $faker = Faker::create("pt-BR");
+        $categorias = [
+            "tecnologia",
+            "mecanico",
+            "eletricista",
+            "pintor",
+            "pedreiro",
+            "jardineiro"
+        ];
+
+        foreach($categorias as $categoria){
+            DB::table('categorias')->insert([
+                "titulo" => $categoria,
+                "imagem" => asset("img/categoria-carro.jpg"),
+                "descricao" => $faker->sentence(),
+            ]);
+        }
+    }
+}
---
+++ database/seeders/UsuarioSeeder.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Faker\Factory as Faker;
+use Illuminate\Support\Facades\Hash;
+
+class UsuarioSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $faker = Faker::create("pt-BR");
+        for($i = 0; $i <= 10; $i++){
+            DB::table("usuarios")->insert([
+            "nome" => $faker->name(), 
+            "email" => $faker->unique()->safeEmail(),
+            "email_verified_at" => now(),
+            "password" => Hash::make("12345678"),
+            "created_at" => now(),
+            "updated_at" => now()
+            ]);
+
+        }
+    }
+}
